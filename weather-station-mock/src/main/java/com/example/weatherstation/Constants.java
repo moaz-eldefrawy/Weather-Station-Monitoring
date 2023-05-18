968e39e607e8421ffde5e589a328165e105ec8ec
Abdallah <abdallah.yossf@gmail.com>
2023-05-18 15:50:20 +0300

    [add] External listener for kafka

--- weather-station-mock/src/main/java/com/example/weatherstation/Constants.java
+++ weather-station-mock/src/main/java/com/example/weatherstation/Constants.java
@@ -1,7 +1,9 @@
 package com.example.weatherstation;
 
 public class Constants {
-  public static final String BOOTSTRAP_SERVERS = "kafka:9092";
+  // If you are running locally but you want to connect to the k8s kafka, set this to localhost:30000
+  // If running in the k8s cluster, set this to kafka:9092
+  public static final String BOOTSTRAP_SERVERS = "kafka:9092"; 
   public static final String GROUP_ID = "test-group";
   public static final String TOPIC = "weather-station-status";
   public static final String HUMIDITY_NOTIFICATION_TOPIC = "humidity-notification";
