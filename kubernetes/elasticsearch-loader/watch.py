e3841e17133bfe69bd47f973e30a24c6e174997b
Abdallah <abdallah.yossf@gmail.com>
2023-05-26 00:53:04 +0300

    [fix] watchdog with polling observer

--- kubernetes/elasticsearch-loader/watch.py
+++ kubernetes/elasticsearch-loader/watch.py
@@ -1,7 +1,7 @@
-from watchdog.observers import Observer
+from watchdog.observers.polling import PollingObserver
 from watchdog.events import FileSystemEventHandler
 import subprocess
-
+import time 
 
 class NewFileHandler(FileSystemEventHandler):
     def on_created(self, event):
@@ -9,10 +9,17 @@ class NewFileHandler(FileSystemEventHandler):
             return
         else:
             print(f"New file created: {event.src_path}")
-            subprocess.run("elasticsearch_loader", "--index", "weather_status", "--es-host",
-                           "http://elasticsearch:9200", "/parquet-data", event.src_path)
+            subprocess.run(["elasticsearch_loader", "--index", "weather_status", "--es-host",
+                           "http://elasticsearch:9200", "parquet", event.src_path])
 
-
-observer = Observer()
+    
+observer = PollingObserver()
 observer.schedule(NewFileHandler(), path='/parquet-data')
 observer.start()
+
+try: 
+  while True:
+    time.sleep(1)
+finally:
+    observer.stop()
+    observer.join()
